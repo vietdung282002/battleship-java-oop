976134a5554a290e522b4e7be934bca27ad6bf82
Dung <vietdung282002@gmail.com>
2022-07-03 16:19:48 +0700

    update

--- battleship-game-master/battleship-standalone-app/src/main/java/org/fxapps/battleship/app/AppEntryPoint.java
+++ battleship-game-master/battleship-standalone-app/src/main/java/org/fxapps/battleship/app/AppEntryPoint.java
@@ -1,14 +1,16 @@
-package org.fxapps.battleship.app;
+package org.fxapps.battleship.apps;
+
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import org.fxapps.battleship.app.ScreenManagerFactory;
+import org.fxapps.battleship.app.screens.ScreenManager;
+
+import javafx.scene.Scene;
 
 
-public class AppEntryPoint extends Application {
+public class Main extends Application {
 
     private static final int WIDTH = 1600;
     private static final int HEIGHT = 1000;
@@ -21,15 +23,16 @@ public class AppEntryPoint extends Application {
     public void start(Stage stage) throws Exception {
         var factory = new ScreenManagerFactory();
         var manager = factory.newScreenManager(WIDTH, HEIGHT);
-        var gameRoot = manager.root(); 
+        var gameRoot = manager.root();
         var scene = new Scene(new StackPane(gameRoot), WIDTH, HEIGHT);
         scene.getStylesheets().add("battleship-style.css");
+        stage.setMaximized(true);
         stage.setScene(scene);
         stage.show();
-        
+
         Runnable resize = () -> manager.resize(scene.getWidth(), scene.getHeight());
         scene.widthProperty().addListener(l -> resize.run());
-        scene.heightProperty().addListener(l -> resize.run());        
+        scene.heightProperty().addListener(l -> resize.run());
     }
 
 }

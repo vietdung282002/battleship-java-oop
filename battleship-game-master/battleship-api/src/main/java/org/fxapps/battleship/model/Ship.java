6a0e76c1703c7174df498e859a4779946034ea83
Dung <vietdung282002@gmail.com>
2022-07-03 16:19:03 +0700

    update

--- battleship-game-master/battleship-api/src/main/java/org/fxapps/battleship/model/Ship.java
+++ battleship-game-master/battleship-api/src/main/java/org/fxapps/battleship/model/Ship.java
@@ -2,11 +2,11 @@ package org.fxapps.battleship.model;
 
 public enum Ship {
 
-    CARRIER("Carrier", 4),
-    BATTLESHIP("Battleship", 3),
-    CRUISER("Cruiser", 2),
+    CARRIER("Carrier", 5),
+    BATTLESHIP("Battleship", 4),
+    CRUISER("Cruiser", 3),
     SUBMARINE("Submarine", 3),
-    DESTROYER("Destroyer", 5);
+    DESTROYER("Destroyer", 2);
 
     final int spaces;
     final String name;

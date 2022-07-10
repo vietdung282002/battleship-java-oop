e922e15e1f5fd46638eaa251d2a5a82ce1d93642
Dung <vietdung282002@gmail.com>
2022-07-10 22:30:43 +0700

    update

--- battleship-game-master/battleship-standalone-app/src/main/java/org/fxapps/battleship/app/screens/LeaderboardScreen.java
+++ battleship-game-master/battleship-standalone-app/src/main/java/org/fxapps/battleship/app/screens/LeaderboardScreen.java
@@ -25,15 +25,34 @@ public class LeaderboardScreen implements Screen {
         lblTop.getStyleClass().add("lbl-app-title");
         lblTop.getStyleClass().add("normal-title");
         borderPane.setTop(lblTop);
-        BorderPane.setMargin(lblTop, new Insets(50, 0, 150, 0));
+        BorderPane.setMargin(lblTop, new Insets(50, 0, 100, 0));
         BorderPane.setAlignment(lblTop, Pos.BOTTOM_CENTER);
 
-        VBox root = new VBox();
+
+
+        Label first=new Label("1. ");
+        Label second=new Label("2. ");
+        Label third=new Label("3. ");
+        Label fourth=new Label("4. ");
+        Label fifth=new Label("5. ");
+
+        first.getStyleClass().add("lbl-score");
+        second.getStyleClass().add("lbl-score");
+        third.getStyleClass().add("lbl-score");
+        fourth.getStyleClass().add("lbl-score");
+        fifth.getStyleClass().add("lbl-score");
+
+
+        VBox root = new VBox(20,first,second,third,fourth,fifth);
+
         borderPane.setCenter(root);
 
+        BorderPane.setAlignment(root, Pos.CENTER);
+
         var btnReset = new Button("Reset");
+        btnReset.getStyleClass().add("btn-reset");
         borderPane.setBottom(btnReset);
-        BorderPane.setMargin(btnReset, new Insets(50, 0, 150, 0));
+        BorderPane.setMargin(btnReset, new Insets(50, 0, 100, 0));
         BorderPane.setAlignment(btnReset, Pos.BOTTOM_CENTER);
     }
 
